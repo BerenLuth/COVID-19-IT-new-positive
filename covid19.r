d35d46b9cb0fa4f601aa5c853dbc5a7da5c931c0
Fabio <r.fabio95@gmail.com>
2020-03-22 22:08:17 +0100

    new labels and new example

--- covid19.r
+++ covid19.r
@@ -3,15 +3,19 @@ for(n in length(covid$totale_casi):2){
   covid$totale_casi[n] = covid$totale_casi[n] - covid$totale_casi[n-1]
 }
 cov.ts<-ts(covid$totale_casi)
-main <- "Andamento giornaliero dei nuovi casi di covid19 in italia"
-ylab<-"Nuovi positivi"
-xlab<-"Giorni"
+main <- 'Daily trend of "New positive" cases of COVID-19 in Italy'
+ylab<-"New positives"
+xlab<-"Days since the beginning of data"
 #png(filename="/home/berenluth/Projects/covid19/export.png", width = 1000, height=500, units="px")
-png(filename="export.png", width = 1000, height=500, units="px")
+png(filename="export.png", width = 1000, height=650, units="px")
 
-plot(cov.ts, ylab=ylab, main=main)
+plot(cov.ts, ylab=ylab, main=main, xlab=xlab)
 points(covid$totale_casi)
 tt<-as.numeric(time(cov.ts))
 fit4<-lm(cov.ts~poly(tt,degree=4,raw=TRUE))
 lines(tt,predict(fit4),col='red',lwd=2)
+legend("bottomright",
+       legend = "First day: 24/02/2020")
+
 #dev.off()
+
