bd00a749f0e81157b8837b2959cb62d38e1841aa
Fabio <r.fabio95@gmail.com>
2020-03-24 19:44:40 +0100

    Plot now includes recovered and deaths

--- covid19.r
+++ covid19.r
@@ -1,21 +1,31 @@
 covid<-read.csv("COVID-19/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv")
 for(n in length(covid$totale_casi):2){
   covid$totale_casi[n] = covid$totale_casi[n] - covid$totale_casi[n-1]
+  covid$deceduti[n] = covid$deceduti[n] - covid$deceduti[n-1]
+  covid$dimessi_guariti[n] = covid$dimessi_guariti[n] - covid$dimessi_guariti[n-1]
 }
+covid$data <- as.Date(covid$data)
 cov.ts<-ts(covid$totale_casi)
-main <- 'Daily trend of "New positive" cases of COVID-19 in Italy'
-ylab<-"New positives"
+main <- 'Daily data of "New positive" cases of COVID-19 in Italy'
+ylab<-"# People"
 xlab<-"Days since the beginning of data"
 #png(filename="/home/berenluth/Projects/covid19/export.png", width = 1000, height=500, units="px")
 png(filename="export.png", width = 1000, height=650, units="px")
 
-plot(cov.ts, ylab=ylab, main=main, xlab=xlab)
-points(covid$totale_casi)
+plot(cov.ts, ylab=ylab, main=main, xlab=xlab, type='l', xaxs='i')
+axis(1, covid$data, format(covid$data, "%b %d"))
+grid()
+#points(covid$totale_casi)
 tt<-as.numeric(time(cov.ts))
 fit4<-lm(cov.ts~poly(tt,degree=4,raw=TRUE))
-lines(tt,predict(fit4),col='red',lwd=2)
-legend("bottomright",
-       legend = "First day: 24/02/2020")
+lines(tt,predict(fit4),col="blue",lwd=1, type="c")
+lines(covid$dimessi_guariti,col="green",lwd=2, type="l")
+lines(covid$deceduti,col="red",lwd=2, type="l")
+
+# legend("bottomright", legend = "First day: 24/02/2020")
+
+legend("topleft",legend = c("New Positive","Recovered","Deaths", "Trend"),
+       col=c("black","green","red", "blue"),lwd=2,lty=c(1,1,1,2)) # lty sets the type of line
 
 #dev.off()
 
