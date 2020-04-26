85ac92236b4b8b4e339b90969341db17fa215e15
Fabio <r.fabio95@gmail.com>
2020-04-26 21:03:17 +0200

    updated submodule

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "downloading the latest data..."
-git submodule update
+cd COVID-19
+git pull
+cd ..
+git add .
+git commit -m "updated submodule"
 echo "Launching r script..."
 Rscript covid19.r && echo "plot generated!"
